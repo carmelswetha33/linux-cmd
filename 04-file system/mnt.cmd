fed9dc167aeb20e4fe406ecdd9a238c32ef92068
Carmel <carmelswetha3@gmail.com>
2026-08-01 22:04:54 +0530

    Create mnt.cmd

---
+++ 04-file system/mnt.cmd
@@ -0,0 +1,6 @@
+--Mount Directory--
+  lsblk --(listblock)how many disk we have
+  mkfs -t xfs (device name) --formation (complete wipe out)
+  /mnt/(dir name) --existing a empty directory
+  mkdir/(dir name) --create a new directory
+  mnt/(filename)/(disk name)/(mount point) --mount the disk
