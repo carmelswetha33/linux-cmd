106e4f550a900650520653cbfea8799853640147
Carmel <carmelswetha3@gmail.com>
2026-08-01 15:48:00 +0530

    Create chmod.cmd

---
+++ file permissions/chmod.cmd
@@ -0,0 +1,9 @@
+chmod --changemode
+--- Permission Types ---
+chmod +x --executable persimssion
+chmod +r --read only permission
+chmod +w --write only permission
+---denied permission---
+chmod -x --denied executable
+chmod -r --remove read permission
+chmod -w --remove write permission
