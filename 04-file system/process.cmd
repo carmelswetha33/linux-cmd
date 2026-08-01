4f4e5ef95dd2a093b1da99c7f26c04e08c9686ef
Carmel <carmelswetha3@gmail.com>
2026-08-01 21:51:05 +0530

    Create process.cmd

---
+++ 04-file system/process.cmd
@@ -0,0 +1,8 @@
+--process information--
+/proc -- live directory
+/proc/cpuinfo --cpu information
+/proc/meminfo --memory information
+   ex: Memtotal
+       Memfree
+       MemAvailable
+/proc/mounts --shows mounted filesystem form the kernels view
